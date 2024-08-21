95e14192a7fc364aa036f8b96dcd09261250513c
Jan Boehm <ucesjo0@users.noreply.github.com>
2024-08-21 15:19:57 +0100

    Create run1.bat

---
+++ CC_commands/run1.bat
@@ -0,0 +1,2 @@
+"c:\Program Files\CloudCompare\CloudCompare.exe" -VERBOSITY 0 -SILENT -COMMAND_FILE command_C2C_DIST.txt
+"c:\Program Files\CloudCompare\CloudCompare.exe" -VERBOSITY 0 -SILENT -COMMAND_FILE command_post_C2C_DIST.txt
